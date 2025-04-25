eae8d9fe603f98f26d426f3073cc88f8bbc51ce4
RomanPepper <70803548+RomanPepper@users.noreply.github.com>
2025-04-25 23:43:25 +0300

    feat: add subscribe actions control
    add license_limits, camera model and migration
    
    QW-111

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,5 +3,6 @@ from .user import User
 from .tariff import Tariff
 from .payment import Payment
 from .license import License
+from .camera import Camera
 
-__all__ = ["Client", "User", "Tariff", "Payment", "License"]
+__all__ = ["Client", "User", "Tariff", "Payment", "License", "Camera"]
