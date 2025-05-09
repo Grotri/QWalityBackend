77494fcb6685e25e789474ff4872f3e12494915e
Grotri <progamer1205mail@gmail.com>
2025-05-09 21:26:55 +0300

    refactor: cleanup code
    QW-153

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import click
 from dotenv import load_dotenv
 from flask import Flask
-from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
-from app.controllers import _build_cors_preflight_response, _corsify_actual_response
 from app.extensions import db, mail, cache
 from app.routes import register_routes
 from tests.dev_seed import seed_test_tariff_and_license
@@ -26,42 +24,12 @@ def create_app():
     app.add_url_rule('/', view_func=option_todo, provide_automatic_options=False, methods=['OPTIONS'])
     app.add_url_rule(r'/<path:path>', view_func=option_todo, provide_automatic_options=False, methods=['OPTIONS'])
 
-    @app.after_request
-    def after_request(response):
-        response.headers['Access-Control-Allow-Methods']='*'
-        response.headers['Access-Control-Allow-Origin']='*'
-        response.headers['Vary']='Origin'
-        return response
-
-    # @app.after_request
-    # def disable_cors(response):
-    #     response.headers["Access-Control-Allow-Origin"] = "*"
-    #     response.headers["Access-Control-Allow-Methods"] = "*"
-    #     response.headers["Access-Control-Allow-Headers"] = "*"
-    #     return response
-
     cache.init_app(app)
     db.init_app(app)
     jwt.init_app(app)
     mail.init_app(app)
 
-    # CORS(app,
-    #      resources={r"/*": {  # Применяем ко всем маршрутам
-    #          "origins": "*",  # Разрешаем запросы с любых доменов
-    #          "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],  # Все HTTP методы
-    #          "allow_headers": "*",  # Все заголовки
-    #          "expose_headers": "*",  # Все заголовки ответа доступны клиенту
-    #          "supports_credentials": True,  # Поддержка cookies и авторизации
-    #          "max_age": 86400  # Кеширование preflight-запросов на 24 часа
-    #      }})
-
-    # CORS(app, support_credentials=True)
-
     register_routes(app)
-
-    # @app.after_request
-    # def wrap_response(response):
-    #     return _corsify_actual_response(response)
 
     @app.cli.command("seed-dev")
     @click.option("--client-id", required=True, type=int, help="Client ID for seeding")
