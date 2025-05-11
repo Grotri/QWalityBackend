baeb84458054f1c30c6489848e0ed816413afedd
RomanPepper <70803548+RomanPepper@users.noreply.github.com>
2025-05-11 15:24:07 +0300

    feat: add minio into docker compost
    QW-173

--- test_db.py
+++
@@ -1,9 +0,0 @@
-# Создайте файл test_db.py
-from sqlalchemy import create_engine
-from app.config import Config
-
-print(f"Attempting to connect to: {Config.SQLALCHEMY_DATABASE_URI}")
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
-connection = engine.connect()
-print("Connection successful!")
-connection.close()
