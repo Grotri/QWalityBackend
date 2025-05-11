034b6be5c85002994ddca787357ebcd093018c06
RomanPepper <70803548+RomanPepper@users.noreply.github.com>
2025-05-11 16:48:19 +0300

    fix: fix unregistering of inspection blueprint
    QW-174

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,6 +3,7 @@ from app.controllers.auth_controller import auth_bp
 from app.controllers.camera_controller import camera_bp
 from app.controllers.client_controller import clients_bp
 from app.controllers.defect_controller import defects_bp
+from app.controllers.inspection_controller import inspection_bp
 from app.controllers.minio_controller import utils_bp
 from app.controllers.payments_controller import payments_bp
 from app.controllers.user_controller import user_bp
@@ -17,3 +18,4 @@ def register_routes(app):
     app.register_blueprint(camera_bp)
     app.register_blueprint(defects_bp)
     app.register_blueprint(utils_bp)
+    app.register_blueprint(inspection_bp)
