e7c935149f56fbc7321db35dbd4ba51c436f4815
RomanPepper <70803548+RomanPepper@users.noreply.github.com>
2025-05-15 20:39:50 +0300

    feat(camera): add deleted_at into camera model
    QW-155

--- app/models/camera.py
+++ app/models/camera.py
@@ -11,7 +11,8 @@ class Camera(db.Model):
     camera_url = db.Column(db.String(255), nullable=True)
 
     client_id = db.Column(db.Integer, db.ForeignKey("client.id"), nullable=False)
-    status = db.Column(db.String(20), default="active")  # active / deleted
+    status = db.Column(db.String(20), default="active")  # active / non-active / deleted
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    deleted_at = db.Column(db.DateTime, default=None, nullable=True)
 
-    client = db.relationship("Client", backref="cameras")
+    client = db.relationship("Client", backref="camera")
